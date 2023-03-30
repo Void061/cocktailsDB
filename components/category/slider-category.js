0e705b4323f4c7618486763c87b3f6d043ffb30b
bubble <bubblegame061@outlook.it>
2023-03-30 17:57:29 +0200

    Last fix

--- components/category/slider-category.js
+++ components/category/slider-category.js
@@ -31,10 +31,10 @@ const SliderCategory = (props) => {
                    
                     decodeCategory(currentCat) != item.strCategory ?
                     <Link onClick={() => handleChangeCat(encodeCategory(item.strCategory))} href={'/category/'+ encodeCategory(item.strCategory)} key={index} scroll={false}>
-                        <li className="transition-all hover:bg-primary hover:text-secondary text-[20px] font-semibold px-6 cursor-pointer rounded-[25px] text-primary bg-secondary" >{item.strCategory}</li>
+                        <li className="transition-all hover:bg-primary hover:text-secondary text-[20px] font-medium px-6 cursor-pointer rounded-[25px] text-primary bg-secondary" >{item.strCategory}</li>
                         
                     </Link> :
-                    <li key={index} className="transition-all text-[20px] font-semibold px-6 cursor-pointer rounded-[25px] text-secondary bg-primary" >{item.strCategory}</li>                 
+                    <li key={index} className="transition-all text-[20px] font-medium px-6 cursor-pointer rounded-[25px] text-secondary bg-primary" >{item.strCategory}</li>                 
                 ))}
             </ul>
             
